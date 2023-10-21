31412b34362b747bbea179a654b850757a8a88cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-21 04:36:28 +0100

    fix: handle close menu bar functionality

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
+const links = document.querySelectorAll('#link');
 const formEl = document.querySelector('#contact');
 const copyrightEl = document.querySelector('#copyright');
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('hidden');
   hamburger.classList.toggle('bg-white');
+});
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    hamburger.classList.toggle('bg-white');
+  });
 });
 
 formEl.addEventListener('submit', (e) => {
