d77ffaa70d53fedded22a2bd2e95543b18d78092
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-21 03:19:49 +0100

    fix: prevent defalut behavior of contact form

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
+const formEl = document.querySelector('#contact');
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('hidden');
   hamburger.classList.toggle('bg-white');
+});
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
 });
