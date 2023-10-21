330c680dda084a0c5d4bcaab9c2d7070e96e04ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-21 16:08:32 +0100

    fix: handle darkmode toggle button functionality

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 const links = document.querySelectorAll('#link');
+const body = document.querySelector('body');
 const formEl = document.querySelector('#contact');
+const moon = document.querySelector('#moon');
 const copyrightEl = document.querySelector('#copyright');
 
 hamburger.addEventListener('click', () => {
@@ -14,6 +16,10 @@ links.forEach((link) => {
     menu.classList.toggle('hidden');
     hamburger.classList.toggle('bg-white');
   });
+});
+
+moon.addEventListener('click', () => {
+  body.classList.toggle('dark');
 });
 
 formEl.addEventListener('submit', (e) => {
